988cbe9f26f14f8a0ef747dfbecb1f0db4430e7a
Yan Santos <yanbrunosilvasantos@gmail.com>
2023-05-25 08:10:51 -0300

    ok

--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
