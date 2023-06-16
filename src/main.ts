f49a765dcd52eebb7e4994838e54296380094898
Yan Santos <yanbrunosilvasantos@gmail.com>
2023-06-16 19:39:28 -0300

    ok

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
-const documentHeight = () => {
-  const doc = document.documentElement
-  doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
-}
-window.addEventListener('resize', documentHeight)
-documentHeight()
