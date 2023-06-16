f32cc73a5e20c285ccdc972ede085f0f7bbf391e
Yan Santos <yanbrunosilvasantos@gmail.com>
2023-06-16 19:46:20 -0300

    ok

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,10 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+const appHeight = () => {
+  const doc = document.documentElement
+  doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', appHeight)
+appHeight()
