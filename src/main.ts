79d550bc69db7ac0a773550df71fed6d165bddf6
Yan Santos <yanbrunosilvasantos@gmail.com>
2023-06-24 12:36:15 -0300

    ok

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
+
+// Define a altura disponível na tela
 const appHeight = () => {
   const doc = document.documentElement
   doc.style.setProperty('--app-height', `${window.innerHeight}px`)
