fb980e72e0219c6ae7b988599cb606b0f4e57a11
Yan Santos <yanbrunosilvasantos@gmail.com>
2023-06-24 14:30:29 -0300

    ok

--- src/app/agenda/services/scheduling.service.ts
+++ src/app/agenda/services/scheduling.service.ts
@@ -9,12 +9,14 @@ import { environment } from 'src/environments/environment';
 })
 export class SchedulingService {
 
-  private urlBase = environment.base_url;
   private schedulingItems = "/v1/Scheduling/items?day=2023-06-10&unitId=4644D6E1-A352-4003-A1D1-B597B5AE1B65";
 
   constructor(private http: HttpClient) { }
 
   getSchedulingItems(): Observable<SchedulingItem[]> {
-    return this.http.get<SchedulingItem[]>(`${this.urlBase}${this.schedulingItems}`).pipe(first());
+
+    console.log(environment.production);
+    console.log(`${environment.base_url}${this.schedulingItems}`);
+    return this.http.get<SchedulingItem[]>(`${environment.base_url}${this.schedulingItems}`).pipe(first());
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    base_url: 'api'
+    production: true,
+    base_url: 'oi'
 };
