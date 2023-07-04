008ca09c29c4ad7aa3e16c688c7cb0358bde2640
Yan Santos <yanbrunosilvasantos@gmail.com>
2023-07-04 05:46:42 -0300

    ok

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MenuItem } from '../../models/menu-item.model';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { UnitService } from '../../services/unit.service';
+import { Security } from '../../utils/security.util';
 
 @Component({
   selector: 'app-navbar',
@@ -15,7 +16,8 @@ export class NavbarComponent {
   ];
 
   constructor(private authService: AuthService, private unitService: UnitService) {
-
+    if (!Security.getUnit())
+      this.unitService.showModal();
   }
 
   logout() {
